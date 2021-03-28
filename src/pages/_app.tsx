9a7dc980d3fac31c7d93e81750dfb94ea5b89080
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-28 11:32:17 -0300

    feat: Add font styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,31 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Geraldo Luiz</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" color="#06092b" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+
         <meta
           name="description"
-          content="A simple project starter to work Typescript, React, NextJS and Styled Components"
+          content="Geraldo Luiz - Sou desenvolvedor front-end com habilidades em HTML, CSS, JS, ReactJS e muito mais."
         />
       </Head>
       <GlobalStyles />
