1cd6a7a0f8f9887201b9058bbdcdf664734ff215
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-28 11:31:41 -0300

    feat: Add styles global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,8 +16,22 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'Mulish', sans-serif;
+    line-height: 2.1;
+    letter-spacing: .8px;
+    word-wrap: break-word;
+    font-weight: 400;
+    background-color: #202022;
+    color: #bbb;
   }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  /* font-family: 'Mulish', sans-serif; */
+  /* font-family: 'Montserrat', sans-serif; */
+  /* font-family: 'Poppins', sans-serif; */
 `
 
 export default GlobalStyles
