476ef2502b7df4e774ef0d27955824fc2ff10e00
Geraldo Luiz <geraldobl58@gmail.com>
2021-04-24 16:37:30 -0300

    fix: styles header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,13 +1,10 @@
 import styled from 'styled-components'
 
 export const HeaderWrapper = styled.header`
-  width: 100%;
   padding: 20px 0;
 `
 
 export const HeaderContent = styled.div`
-  max-width: 1400px;
-  width: 100%;
   margin: 0 auto;
   padding: 0 40px;
 
