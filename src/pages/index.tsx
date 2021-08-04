c9bb9ac009eb52e7555f365968196ffe3f734cc2
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-04 18:25:05 -0300

    :fire: Removing section portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import Header from 'components/Header'
 import Hero from 'components/Hero'
-import Portfolio from 'components/Portfolio'
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      <Portfolio />
     </>
   )
 }
